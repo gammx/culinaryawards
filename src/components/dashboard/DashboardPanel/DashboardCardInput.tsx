411a40db515bac6367e958d7f874b9f3b206c4bf
GAMMX <dakdaker55@gmail.com>
2023-03-16 11:44:37 -0700

    fix: stop DashboardCardInput passing boolean prop as attribute

--- src/components/dashboard/DashboardPanel/DashboardCardInput.tsx
+++ src/components/dashboard/DashboardPanel/DashboardCardInput.tsx
@@ -7,13 +7,14 @@ interface DashboardCardInputProps extends React.InputHTMLAttributes<HTMLInputEle
 
 const DashboardCardInput: React.FC<DashboardCardInputProps> = ({
 	className,
+	outlined,
 	...props
 }) => {
 	return (
 		<input
 			spellCheck={false}
-			className={cn("bg-transparent border border-[#0A1419] outline-[#0A1419] focus:outline focus:outline-offset-2 focus:outline-[#0A1419]/50 w-full h-8 px-5 py-3 text-sm text-[#96B1BA] transition-all duration-150", className, {
-				"bg-[#081116]/30": !props.outlined,
+			className={cn("bg-transparent rounded-md border border-[#0A1419] outline-[#0A1419] focus:outline focus:outline-offset-2 focus:outline-[#0A1419]/50 w-full h-8 px-5 py-3 text-sm text-[#96B1BA] transition-all duration-150", className, {
+				"bg-[#081116]/30": !outlined,
 			})}
 			{...props}
 		/>
